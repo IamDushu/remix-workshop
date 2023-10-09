260b001d4467a754e67690fa1dadfa735441ea21
IamDushu <hey.dushyanth@gmail.com>
2023-10-09 15:55:17 +0530

    Added prisma data fetch

--- exercise/02-data-loading/app/routes/posts/index.tsx
+++ exercise/02-data-loading/app/routes/posts/index.tsx
@@ -1,17 +1,9 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, Link } from "@remix-run/react";
+import { prisma } from "~/db.server";
 
 async function getPosts() {
-  return [
-    {
-      slug: "my-first-post",
-      title: "My First Post",
-    },
-    {
-      slug: "90s-mixtape",
-      title: "A Mixtape I Made Just For You",
-    },
-  ];
+  return prisma.post.findMany();
 }
 
 //runs only on the server
