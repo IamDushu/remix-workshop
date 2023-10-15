631a90d65564c08b85d07fb088e369b9c0a56c15
Chaithanya-git <chaithanyagali8@gmail.com>
2023-10-15 22:14:28 +0530

    Dynamic params

---
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -0,0 +1,20 @@
+import { LoaderArgs } from '@remix-run/server-runtime';
+import { json } from "@remix-run/node"
+import { getPost } from '~/models/post.server';
+import { useLoaderData } from '@remix-run/react';
+import { marked } from "marked";
+
+
+export async function loader({params}: LoaderArgs) {
+  const post =  await getPost(params.slug)
+  return json({post: post})
+}
+
+export default function post() {
+  const data = useLoaderData<typeof loader>()
+  return (
+    <main className="mx-auto max-w-4xl">
+      <h1 className="my-6 border-b-2 text-center text-3xl">{data.post.title}</h1>
+    </main>
+  )
+}
