4f177e281bbc7b9553ec8a166a8411875f701222
IamDushu <hey.dushyanth@gmail.com>
2023-10-15 22:34:51 +0530

    markdown to html

--- exercise/03-dynamic-params/app/routes/posts/$slug.tsx
+++ exercise/03-dynamic-params/app/routes/posts/$slug.tsx
@@ -13,14 +13,24 @@ export async function loader({params}: LoaderArgs) {
   if(!post) {
     throw new Error("Post not found")
   }
+
   return json({post: post})
 }
 
 export default function post() {
   const data = useLoaderData<typeof loader>()
+  const html = marked(data.post.markdown);
   return (
     <main className="mx-auto max-w-4xl">
-      <h1 className="my-6 border-b-2 text-center text-3xl">{data.post.title}</h1>
+      <h1 className="my-6 border-b-2 text-center text-3xl">
+        {data.post.title}
+      </h1>
+
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+
+      {/* This doesn't work xss; react, angular etc: escape it for us */}
+      {/* <div>{html}</div> */}
+      {/* <div>{`<script>alert("Gotcha!")</script>`}</div> */}
     </main>
-  )
+  );
 }
